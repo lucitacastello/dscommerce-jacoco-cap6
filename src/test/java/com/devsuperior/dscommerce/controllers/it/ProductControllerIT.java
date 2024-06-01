8454750b091aaad07c5464a6660dc1f51e57bb22
cidarosa <lucita.rosa@gmail.com>
2024-06-01 18:42:15 -0300

    init

---
+++ src/test/java/com/devsuperior/dscommerce/controllers/it/ProductControllerIT.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dscommerce.controllers.it;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+//Teste de Integração
+@SpringBootTest //carrega contexto da app
+@AutoConfigureMockMvc
+@Transactional
+public class ProductControllerIT {
+}
