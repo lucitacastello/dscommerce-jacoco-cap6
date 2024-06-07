e6eca47a601954945767835a0f8491c09a54cfb1
cidarosa <lucita.rosa@gmail.com>
2024-06-07 16:26:49 -0300

    Problema 2: 1-insere produto com dados válidos logado admin

--- src/test/java/com/devsuperior/dscommerce/controllers/it/ProductControllerIT.java
+++ src/test/java/com/devsuperior/dscommerce/controllers/it/ProductControllerIT.java
@@ -1,5 +1,10 @@
 package com.devsuperior.dscommerce.controllers.it;
 
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.Category;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.tests.TokenUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +13,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -23,14 +30,44 @@ public class ProductControllerIT {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private TokenUtil tokenUtil; //obter token
+
+    @Autowired
+    private ObjectMapper objectMapper; //para usar body da requisição
+
     private String productName;
+
+    private String adminToken, clientToken, invalidToken;
+    private String adminUserName, adminPassword, clientUserName, clientPassword;
+
+    //para usanr no insert
+    private Product product;
+    private ProductDTO productDTO;
 
     @BeforeEach
     void setUp() throws Exception {
 
+        adminUserName = "alex@gmail.com";
+        adminPassword = "123456";
+
+        clientUserName = "maria@gmail.com";
+        clientPassword = "123456";
+
         productName = "Macbook";
 
+        adminToken = tokenUtil.obtainAccessToken(mockMvc, adminUserName, adminPassword); //obtem token admin
+        clientToken = tokenUtil.obtainAccessToken(mockMvc, clientUserName, clientPassword); //obtem tokem client
 
+        invalidToken = adminToken + "xpto"; //simulando password inválida - wrong password
+
+        // inicializando product e productDTO
+        Category category = new Category(2L, "Eletro");
+        product = new Product(null, "Console PlayStation 5", "Lorem ipsum, dolor sit amet consectetur adipisicing elit.", 3999.90, "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg");
+        //associando categoria ao produto
+        product.getCategories().add(category);
+
+        productDTO = new ProductDTO(product);
     }
 
     @Test
@@ -57,5 +94,29 @@ public class ProductControllerIT {
         result.andExpect(jsonPath("$.content[0].name").value("The Lord of the Rings"));
         result.andExpect(jsonPath("$.content[0].price").value(90.5));
         result.andExpect(jsonPath("$.content[0].imgUrl").value("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"));
+    }
+
+    //    Problema 2: Inserir produto
+    @Test
+    public void insertShouldRetunrProductDTOCreatedWhenAdminLogged() throws Exception {
+//      1.	Inserção de produto insere produto com dados válidos quando logado como admin
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+        ResultActions result = mockMvc
+                .perform(post("/products")
+                        .header("Authorization", "Bearer " + adminToken) //obter token de admin
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print()); //debugar
+
+        result.andExpect(status().isCreated());
+        result.andExpect(jsonPath("$.id").value(26L)); //tem 25 na lista
+        result.andExpect(jsonPath("$.name").value("Console PlayStation 5"));
+        result.andExpect(jsonPath("$.description").value("Lorem ipsum, dolor sit amet consectetur adipisicing elit."));
+        result.andExpect(jsonPath("$.price").value(3999.90));
+        result.andExpect(jsonPath("$.imgUrl").value("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"));
+        result.andExpect(jsonPath("$.categories[0].id").value(2L));
+
+
     }
 }
